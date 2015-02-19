9eeb611517a1b09686cc75164c9e2a3bde7004aa
Don Brower <dbrower@nd.edu>
2015-02-19 12:56:40 -0500

    fixing path for real. need to remove one more component

--- script/start-server.sh
+++ script/start-server.sh
@@ -7,6 +7,6 @@ app_root=$(cd $(dirname $0)/.. && pwd)
 source /etc/profile.d/chruby.sh
 chruby 2.0.0-p353
 
-source $app_root/current/script/get-env.sh
+source $app_root/script/get-env.sh
 cd $RAILS_ROOT
 bundle exec unicorn -D -E deployment -c $RAILS_ROOT/config/unicorn.rb
