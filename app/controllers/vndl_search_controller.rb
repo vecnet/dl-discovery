9b8f84fe58ff936d94c1ee96513e0625a6fbc581
Steve Vandervalk <steven.vandervalk@gmail.com>
2015-02-20 11:44:43 +1000

    Add no layout application controller for search results, vndl-search-controller

---
+++ app/controllers/vndl_search_controller.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+require 'blacklight/catalog'
+
+class VndlSearchController < CatalogController
+
+  layout :pick_layout
+
+  def pick_layout
+
+    if has_search_parameters?
+      return 'vndl-search-results'
+    else
+      "vndl"
+    end
+  end
+
+
+end
