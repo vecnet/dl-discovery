7ae42723227c587aeabdbaa430439332db4552db
Don Brower <dbrower@nd.edu>
2015-05-11 11:56:07 -0400

    Add a lock to the harvest process
    This ensures there is never more than one harvest running at a time.

--- script/harvest-cron-job.sh
+++ script/harvest-cron-job.sh
@@ -8,6 +8,13 @@ DISCOVERY_SOLR="http://localhost:8081/solr/discovery"
 
 app_root=$(cd $(dirname $0)/.. && pwd)
 
+# Try to get a lock, and exit if someone else already has it.
+# This keeps a lot of harvest processes from spawning
+# should a paricular harvest take a long time.
+# The lock is released when this shell exits.
+exec 200> "$app_root/tmp/last-harvest-$which-lock"
+flock -e --nonblock || exit 0
+
 # mark this time
 new_harvest_mark=$(date '+%Y-%m-%dT%H:%M:%S')
 
