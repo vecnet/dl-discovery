a0b500581611f20ddf096b456d63b1c2c09802d9
Steven van der valk <steven.vandervalk@gmail.com>
2015-06-05 13:42:30 +1000

    Wahahahadslfka

--- app/assets/javascripts/geoblacklight.js
+++ app/assets/javascripts/geoblacklight.js
@@ -1,5 +1,6 @@
 //= require geoblacklight/geoblacklight
 //= require geoblacklight/viewers
+//= require geoblacklight/basemaps
 //= require geoblacklight/modules
 //= require geoblacklight/downloaders
 //= require blacklight_range_limit
