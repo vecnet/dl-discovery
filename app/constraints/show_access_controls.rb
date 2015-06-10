13c785ce16a4d654110a85b36c0634053c51b601
Don Brower <dbrower@nd.edu>
2015-06-10 10:44:14 -0400

    Add authz
    1. Add search filters on permissions
    2. Add access enforcement on show pages

---
+++ app/constraints/show_access_controls.rb
@@ -0,0 +1,30 @@
+class AccessDenied < RuntimeError
+end
+
+module ShowAccessControls
+
+  # intended to be used to guard the show method in the blacklight
+  # catalog controller.
+  def enforce_show_permissions(opts={})
+    doc = fetch params[:id]
+    ok = false
+    if current_user
+      ok ||= current_user.admin?
+      ok ||= member_of_field(doc["read_access_group_sm"], ["public", "registered"] + current_user.groups)
+      ok ||= member_of_field(doc["edit_access_group_sm"], current_user.groups)
+      ok ||= member_of_field(doc["read_access_person_sm"], current_user.uid)
+      ok ||= member_of_field(doc["edit_access_person_sm"], current_user.uid)
+    else
+      ok = member_of_field(doc["read_access_group_sm"], "public")
+    end
+    raise AccessDenied unless ok
+  end
+
+  private
+  def member_of_field(field, item)
+    return false if field.nil?
+    return (field & item).length > 0 if item.is_a?(Array)
+    field.include?(item)
+  end
+
+end
