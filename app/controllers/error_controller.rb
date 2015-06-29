6ac5d4fcd7ef5790f863b5b64f40ea69cd72ff61
Steven van der valk <steven.vandervalk@gmail.com>
2015-06-29 12:10:41 +1000

    Remove the unnecessary Routing error catchall as Rails already 500's and that reaches a friendly page.

--- app/controllers/error_controller.rb
+++
@@ -1,6 +0,0 @@
-class ErrorsController < ApplicationController
-
-  def catch_404
-    raise ActionController::RoutingError.new(params[:path])
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,6 @@ Rails.application.routes.draw do
   resource :feedback_form, path: 'feedback', only: [:new, :create]
   get 'feedback' => 'feedback_forms#new'
 
-
-  #define a catchall route so Rails will send the request or render the public/404.html page (in production)
-  match "*path", to: "errors#catch_404", via: :all
-
   resources :suggest, only: :index, defaults: { format: 'json' }
 
   # since there is no pubtkt login for development
