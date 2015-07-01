b90d19de9aa7ef2c1f05aef35ef92b0948f0932a
Steven van der valk <steven.vandervalk@gmail.com>
2015-07-01 17:07:19 +1000

    Running out of ideas.

--- app/assets/javascripts/modules/hierarchy.js
+++ app/assets/javascripts/modules/hierarchy.js
@@ -1,25 +1,25 @@
-$('#ajax-modal').on('shown.bs.modal', function() {
+Blacklight.onLoad(function () {
   Blacklight.do_hierarchical_facet_expand_contract_behavior();
-    console.log('window ready event fired, modal shown event fired and called initial function');
+  alert('window ready event fired, modal shown event fired and called initial function');
 });
 
-(function($) {
-  Blacklight.do_hierarchical_facet_expand_contract_behavior = function() {
-    $( Blacklight.do_hierarchical_facet_expand_contract_behavior.selector ).each (
-        Blacklight.hierarchical_facet_expand_contract
-     );
-      console.log('print for each selector expand contract function call');
+(function ($) {
+  Blacklight.do_hierarchical_facet_expand_contract_behavior = function () {
+    $(Blacklight.do_hierarchical_facet_expand_contract_behavior.selector).each(
+      Blacklight.hierarchical_facet_expand_contract()
+    );
   };
   Blacklight.do_hierarchical_facet_expand_contract_behavior.selector = 'li.h-node';
 
-  Blacklight.hierarchical_facet_expand_contract = function() {
-      console.log("inside expand contract function");
+  Blacklight.hierarchical_facet_expand_contract = function () {
+    console.log("inside expand contract function");
+    alert("inside expand contract function");
+
     var li = $(this);
-    
-    $('ul', this).each(function() {
+    $('ul', this).each(function () {
       li.addClass('twiddle');
-        console.log('added twiddle class to li');
-      if($('span.selected', this).length == 0){
+      console.log('added twiddle class to li');
+      if ($('span.selected', this).length == 0) {
         $(this).hide();
       } else {
         li.addClass('twiddle-open');
@@ -27,12 +27,12 @@ $('#ajax-modal').on('shown.bs.modal', function() {
     });
 
     // attach the toggle behavior to the li tag
-    li.click(function(e){
-      if (e.target == this) {
+    li.click(function (e) {
+      if (e.target === this) {
         // toggle the content
         $(this).toggleClass('twiddle-open');
         $(this).children('ul').slideToggle();
       }
     });
   };
-})(jQuery);
+})($);
