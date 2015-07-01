a8b1f6ac93e6c81b8a306485eebb700d7b953c42
Steven van der valk <steven.vandervalk@gmail.com>
2015-07-01 22:03:28 +1000

    Don says.

--- app/assets/javascripts/modules/hierarchy.js
+++ app/assets/javascripts/modules/hierarchy.js
@@ -1,20 +1,23 @@
-(function ($) {
-  Blacklight.onLoad(function () {
-    Blacklight.do_hierarchical_facet_expand_contract_behavior();
-    alert('window ready event fired, modal shown event fired and called initial function');
-  });
+Blacklight.onLoad(function(){
+  Blacklight.do_hierarchical_facet_expand_contract_behavior();
+});
 
+(function($) {
+  Blacklight.do_hierarchical_facet_expand_contract_behavior = function() {
+    $( Blacklight.do_hierarchical_facet_expand_contract_behavior.selector ).each (
+      Blacklight.hierarchical_facet_expand_contract
+    );
+  }
   Blacklight.do_hierarchical_facet_expand_contract_behavior.selector = 'li.h-node';
 
-  Blacklight.hierarchical_facet_expand_contract = function () {
-    console.log("inside expand contract function");
-    alert("inside expand contract function");
+  Blacklight.hierarchical_facet_expand_contract = function() {
 
+    alert("printf should be called once");
     var li = $(this);
-    $('ul', this).each(function () {
+
+    $('ul', this).each(function() {
       li.addClass('twiddle');
-      console.log('added twiddle class to li');
-      if ($('span.selected', this).length === 0) {
+      if($('span.selected', this).length == 0){
         $(this).hide();
       } else {
         li.addClass('twiddle-open');
@@ -22,18 +25,12 @@
     });
 
     // attach the toggle behavior to the li tag
-    li.click(function (e) {
-      if (e.target === this) {
+    li.click(function(e){
+      if (e.target == this) {
         // toggle the content
         $(this).toggleClass('twiddle-open');
         $(this).children('ul').slideToggle();
       }
     });
-  };
-
-  Blacklight.do_hierarchical_facet_expand_contract_behavior = function () {
-    $(Blacklight.do_hierarchical_facet_expand_contract_behavior.selector).each(
-      Blacklight.hierarchical_facet_expand_contract()
-    );
   };
 })(jQuery);
