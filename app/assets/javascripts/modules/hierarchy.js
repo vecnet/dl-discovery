2cbf4c653e5733ad2293fccb2e7c5dbb0fea5814
Steven van der valk <steven.vandervalk@gmail.com>
2015-07-01 15:37:38 +1000

    Correction; the jQuery way

--- app/assets/javascripts/modules/hierarchy.js
+++ app/assets/javascripts/modules/hierarchy.js
@@ -1,4 +1,4 @@
-window.onload(function(){
+$(window).onload(function(){
   Blacklight.do_hierarchical_facet_expand_contract_behavior();
     console.log('ran hierarchy facet code on Blacklight load');
     alert('Blacklight load');
