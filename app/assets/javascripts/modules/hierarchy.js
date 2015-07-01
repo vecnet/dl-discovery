9a252464d4057f7557ca361c11de82a44ff5553c
Steven van der valk <steven.vandervalk@gmail.com>
2015-07-01 22:34:55 +1000

    li was correct and works executed in console. so back to order of calling this code?

--- app/assets/javascripts/modules/hierarchy.js
+++ app/assets/javascripts/modules/hierarchy.js
@@ -2,13 +2,14 @@ Blacklight.onLoad(function(){
   Blacklight.do_hierarchical_facet_expand_contract_behavior();
 });
 
+Blacklight.do_hierarchical_facet_expand_contract_behavior.selector = 'li.h-node';
+
 (function($) {
   Blacklight.do_hierarchical_facet_expand_contract_behavior = function() {
     $( Blacklight.do_hierarchical_facet_expand_contract_behavior.selector ).each (
       Blacklight.hierarchical_facet_expand_contract
     );
-  }
-  Blacklight.do_hierarchical_facet_expand_contract_behavior.selector = 'li.h-node';
+  };
 
   Blacklight.hierarchical_facet_expand_contract = function() {
 
@@ -16,17 +17,17 @@ Blacklight.onLoad(function(){
     var li = $(this);
 
     $('ul', this).each(function() {
-      this.addClass('twiddle');
-      if($('span.selected', this).length == 0){
+      li.addClass('twiddle');
+      if($('span.selected', this).length === 0){
         $(this).hide();
       } else {
-        this.addClass('twiddle-open');
+        li.addClass('twiddle-open');
       }
     });
 
     // attach the toggle behavior to the li tag
     li.click(function(e){
-      if (e.target == this) {
+      if (e.target === this) {
         // toggle the content
         $(this).toggleClass('twiddle-open');
         $(this).children('ul').slideToggle();
